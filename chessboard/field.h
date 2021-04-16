fbeb6145ec740421485b3f3ebe4a673acb7fbb63
Sibe Bleuzé <sibebleuze@gmail.com>
2021-04-16 15:56:33 +0200

    initiation of (empty) board, line and field classes for the graphic representation of the chessboard and a move class for recognizing short hand chess notation (future stage)

---
+++ chessboard/field.h
@@ -0,0 +1,10 @@
+#ifndef CHESS_FIELD_H
+#define CHESS_FIELD_H
+
+
+class Field {
+
+};
+
+
+#endif //CHESS_FIELD_H
---
+++ chessboard/line.h
@@ -0,0 +1,10 @@
+#ifndef CHESS_LINE_H
+#define CHESS_LINE_H
+
+
+class Line {
+
+};
+
+
+#endif //CHESS_LINE_H
