16108c7511a9223328997ccffff89bedd29e069d
Sibe Bleuzé <sibebleuze@gmail.com>
2021-04-20 11:20:43 +0200

    replaced deprecated QApplication::desktop()->availableGeometry() by QGuiApplication::screens()[0]->availableGeometry()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow/mainwindow.h"
 #include <QApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow w; // TODO: put comments everywhere
     w.setMinimumSize(1000, 600);
-    w.move(QApplication::desktop()->availableGeometry().center() - w.rect().center()); // window is centered on startup
+    w.move(QGuiApplication::screens()[0]->availableGeometry().center() -
+           w.rect().center()); // window is centered on startup
     w.showMaximized(); // window is opened maximized on startup
     return app.exec();
 }
